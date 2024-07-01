191e24d9678e7cef9b3339a8e7d477fc48347bb3
jisse154 <jisse154@gmail.com>
2024-07-01 14:35:04 +0200

    1st draft smart contract

--- backend/contracts/I4TKdocToken.sol
+++ backend/contracts/I4TKdocToken.sol
@@ -2,85 +2,185 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity 0.8.24;
 
-
-
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract I4TKdocToken is  ERC1155, ERC1155URIStorage, AccessControl, ERC1155Burnable , ERC1155Supply {
+contract I4TKdocToken is
+    ERC1155,
+    ERC1155URIStorage,
+    AccessControl,
+    ERC1155Burnable,
+    ERC1155Supply
+{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 public constant DECIMAL = 6;
+    uint256 public constant tokenMaxSupply=100*1e6;
 
-    constructor(address defaultAdmin, address minter) ERC1155("") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
+    struct Contribution {
+        uint256 TokenId;
+        uint256 Weight;
     }
-    
+
+    mapping(uint256 => Contribution[]) public contributions;
+    mapping(uint256 => address) public creator;
+
     uint256 private _tokenIdCounter;
 
-
-    struct Item {
-        uint256 id;
-        address creator;
-        uint256 _quantity;
-        string uri;
-
+    constructor(address defaultAdmin) ERC1155("") {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        //_grantRole(MINTER_ROLE, minter);
     }
 
-    // mapping (uint256 => Item) public Items;
+    function uri(
+        uint256 tokenId
+    ) public view override(ERC1155, ERC1155URIStorage) returns (string memory) {
+        return ERC1155URIStorage.uri(tokenId);
+    }
 
-    // function createItem(uint256 _quantity, string memory uri) public returns (uint256) {
-
-    //     uint256 tokenId = _tokenIdCounter;
-    //     _mint(msg.sender, newItemId, _quantity, "");
-
-    //     Items[newItemId] = Item(newItemId, msg.sender, _quantity, uri);
-
-    //     return newItemId;
-    // }
-
-    function uri(uint256 tokenId) public view override(ERC1155, ERC1155URIStorage) returns (string memory) {
-    return ERC1155URIStorage.uri(tokenId);
-}
-
-    function mint(address account, uint256 amount,string memory tokenURI, bytes memory data)
-        public
-        onlyRole(MINTER_ROLE)
-    {
+    function mint(
+        address account,
+        string memory tokenURI,
+        uint256[] memory references,
+        bytes memory data
+    ) public onlyRole(MINTER_ROLE) returns (uint256) {
         uint256 tokenId = _tokenIdCounter;
-        _mint(account, tokenId, amount, data);
-        ERC1155URIStorage._setURI(tokenId,tokenURI);
+        _mint(account, tokenId, tokenMaxSupply, data);
+        ERC1155URIStorage._setURI(tokenId, tokenURI);
+        _contributionDefinition(tokenId, references);
+        creator[tokenId]=tx.origin;
         _tokenIdCounter += 1;
+        return tokenId;
+    }
+
+  
+
+    function _contributionDefinition(
+        uint256 _tokenId,
+        uint256[] memory _references
+    ) internal virtual {
+        
+        Contribution memory tokenIdContrib;
+
+        if (_references.length == 0) {
+            tokenIdContrib = Contribution(_tokenId, 1 * 1e6);
+            contributions[_tokenId].push(tokenIdContrib);
+        } else {
+            tokenIdContrib = Contribution(_tokenId, 0.4 * 1e6);
+
+            contributions[_tokenId].push(tokenIdContrib);
+            
+
+            uint256 nbOfRef = _references.length;
+
+            for (uint256 i = 0; i < nbOfRef; i++) {
+                uint256 refTokenID = _references[i];
+
+                for (uint256 y = 0; y < contributions[refTokenID].length; y++) {
+                    Contribution memory Contrib = Contribution(
+                        contributions[refTokenID][y].TokenId,
+                        contributions[refTokenID][y].Weight * 3 / 5 /nbOfRef
+                    );
+                    contributions[_tokenId].push(Contrib);
+                    
+                }
+            }
+
+        }
     }
 
     // function setURI(uint256 tokenId, string memory tokenURI) public  {
     //     ERC1155URIStorage._setURI(tokenId,tokenURI);
     // }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyRole(MINTER_ROLE)
-    {
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyRole(MINTER_ROLE) {
         _mintBatch(to, ids, amounts, data);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
-        internal
-        override(ERC1155, ERC1155Supply)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values
+    ) internal override(ERC1155, ERC1155Supply) {
         super._update(from, to, ids, values);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC1155, AccessControl)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC1155, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
+
+    function formatTokenURI(
+        string memory _CID,
+        string memory _title,
+        string memory _authors,
+        string memory _description,
+        string memory _programme,
+        string memory _category
+    ) public view returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "I4TK document Token #',
+                                _tokenIdCounter,
+                                '"',
+                                ',"description": "A content proposed by a member of the I4KT network community", "content": "ipfs://',
+                                _CID,
+                                '"',
+                                ',"properties": {"title": "',
+                                _title,
+                                '","authors":"',
+                                _authors,
+                                '","description": "',
+                                _description,
+                                '","programme": "',
+                                _programme,
+                                '","category": "',
+                                _category,
+                                '"}}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function getcontributions(uint256 tokenId, uint256 size) external view returns(uint256[2][] memory) {
+        
+        uint256[2][] memory _result =new uint256[2][](size);
+        for( uint256 i =0; i < contributions[tokenId].length; i++) {
+
+            //_result.push([contributions[tokenId][i].TokenId,contributions[tokenId][i].Weight]);
+                _result[i][0]=contributions[tokenId][i].TokenId;
+                _result[i][1]=contributions[tokenId][i].Weight;
+
+
+        }
+
+        return _result;
+
+    }
+
+        function getLengthContrib ( uint256 tokenId) external view returns (uint256) {
+        return contributions[tokenId].length;
+    }
+
+    
+
 }
--- backend/contracts/i4tKnetwork.sol
+++ backend/contracts/i4tKnetwork.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "./I4TKdocToken.sol";
+
 
 
 contract I4TKNetwork is Ownable, AccessControl, ERC1155Holder {
@@ -15,6 +17,17 @@ contract I4TKNetwork is Ownable, AccessControl, ERC1155Holder {
         admin
     }
 
+    enum Status {
+        proposed,
+        validated,
+        trash,
+        removed
+    }
+
+
+    
+    address public I4TKdocTokenAddr;
+    I4TKdocToken token;
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     //bytes32 public constant READONLY_ROLE = keccak256("READONLY_ROLE");
     bytes32 public constant CONTRIBUTOR_ROLE = keccak256("CONTRIBUTOR_ROLE");
@@ -25,13 +38,26 @@ contract I4TKNetwork is Ownable, AccessControl, ERC1155Holder {
         bool isMember;
     }
 
+    struct Validator {
+        address validatorAddr;
+        bool hasValidated;
+    }
+
+    mapping (uint256=> Validator) public validator;
 
     event memberRegistered(address _addr, Profiles  _profile);
+    event contentProposed(address _addr, uint256 _tokenID );
+    event contentValidation(address _addr, uint256 _tokenID);
 
     mapping(address => MetadataOfMember) public Members;
-    mapping(address => uint256) applyAddrIndex;
+    mapping(uint256 => Status) public status;
+    mapping(uint256 => uint256) public nbValidation;
 
-    constructor() Ownable(msg.sender) {
+
+    constructor(address _I4TKdocTokenAddr) Ownable(msg.sender) {
+
+        I4TKdocTokenAddr=_I4TKdocTokenAddr;
+        token=I4TKdocToken(I4TKdocTokenAddr);
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
         _setRoleAdmin(CONTRIBUTOR_ROLE, ADMIN_ROLE);
@@ -98,6 +124,7 @@ contract I4TKNetwork is Ownable, AccessControl, ERC1155Holder {
 
         if (_profile == Profiles.researcher) {
             grantRole(CONTRIBUTOR_ROLE, _addr);
+            token.grantRole(keccak256("MINTER_ROLE"),_addr);
 
             //add token roles
         }
@@ -105,6 +132,7 @@ contract I4TKNetwork is Ownable, AccessControl, ERC1155Holder {
         if (_profile == Profiles.labs) {
             grantRole(CONTRIBUTOR_ROLE, _addr);
             grantRole(VALIDATOR_ROLE, _addr);
+            token.grantRole(keccak256("MINTER_ROLE"),_addr);
 
             //add token roles
 
@@ -112,6 +140,7 @@ contract I4TKNetwork is Ownable, AccessControl, ERC1155Holder {
 
         if (_profile == Profiles.admin) {
             grantRole(ADMIN_ROLE, _addr);
+            token.grantRole(keccak256("DEFAULT_ADMIN_ROLE"),_addr);
         }
 
         Members[_addr].profile= _profile;
@@ -121,5 +150,58 @@ contract I4TKNetwork is Ownable, AccessControl, ERC1155Holder {
         emit memberRegistered(_addr, _profile);
     }
 
-    //function proposeContent() {}
+    function proposeContent(string memory _tokenURI,uint256[] memory _references) external onlyRole(CONTRIBUTOR_ROLE) {
+
+        bytes memory data;
+        address _creator;
+        _creator=msg.sender;
+        uint256 tokenId = token.mint( address(this) ,_tokenURI,_references, data);
+        nbValidation[tokenId]=0;
+        status[tokenId]=Status.proposed;
+        emit contentProposed(_creator, tokenId);
+
+
+    }
+
+
+    function validation(uint256 _tokenId) external onlyRole(VALIDATOR_ROLE) {
+        require(!validator[_tokenId].hasValidated, "You have already validated this content");
+
+
+        nbValidation[_tokenId]++;
+        Validator memory _validator;
+        _validator.validatorAddr=msg.sender;
+        _validator.hasValidated=true;
+
+        validator[_tokenId]=_validator;
+        if (nbValidation[_tokenId] ==4) {
+            _distribution(_tokenId);
+        }
+
+        emit contentValidation(msg.sender, _tokenId);
+
+
+    }  
+
+
+    function _distribution(uint _tokenId) private onlyRole(VALIDATOR_ROLE){
+
+        uint256 nbContrib=token.getLengthContrib(_tokenId);
+
+        uint256[2][] memory contribList = new uint256[2][](nbContrib);
+        contribList=token.getcontributions(_tokenId, nbContrib);
+
+        for(uint256 i=0; i<contribList.length; i++) {
+            uint256 tokenIdToSend= contribList[i][0];
+            address _to;
+            _to = token.creator(tokenIdToSend);
+            uint256 _value=token.balanceOf(address(this),tokenIdToSend) * contribList[i][1] / 1e6;
+            token.safeTransferFrom(address(this), _to, tokenIdToSend, _value,"");
+
+        }
+
+
+    }
+    //function trashEmpty() 
+
 }
